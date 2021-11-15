5f31ed3bbb4bec603dbe92c9b49772e2505def68
liuxiaochen712 <51482732+liuxiaochen712@users.noreply.github.com>
2021-11-15 10:28:18 +0800

    Add files via upload

---
+++ LxcApp/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace LxcApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.显示ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.柳笑辰的GithubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.柳笑辰的个人网站ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.画图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.示例ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.自由画图ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.显示ToolStripMenuItem,
+            this.画图ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(882, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 显示ToolStripMenuItem
+            // 
+            this.显示ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.柳笑辰的GithubToolStripMenuItem,
+            this.柳笑辰的个人网站ToolStripMenuItem});
+            this.显示ToolStripMenuItem.Name = "显示ToolStripMenuItem";
+            this.显示ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.显示ToolStripMenuItem.Text = "显示";
+            // 
+            // 柳笑辰的GithubToolStripMenuItem
+            // 
+            this.柳笑辰的GithubToolStripMenuItem.Name = "柳笑辰的GithubToolStripMenuItem";
+            this.柳笑辰的GithubToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.柳笑辰的GithubToolStripMenuItem.Text = "柳笑辰的Github";
+            this.柳笑辰的GithubToolStripMenuItem.Click += new System.EventHandler(this.柳笑辰的GithubToolStripMenuItem_Click);
+            // 
+            // 柳笑辰的个人网站ToolStripMenuItem
+            // 
+            this.柳笑辰的个人网站ToolStripMenuItem.Name = "柳笑辰的个人网站ToolStripMenuItem";
+            this.柳笑辰的个人网站ToolStripMenuItem.Size = new System.Drawing.Size(212, 26);
+            this.柳笑辰的个人网站ToolStripMenuItem.Text = "柳笑辰的个人网站";
+            this.柳笑辰的个人网站ToolStripMenuItem.Click += new System.EventHandler(this.柳笑辰的个人网站ToolStripMenuItem_Click);
+            // 
+            // 画图ToolStripMenuItem
+            // 
+            this.画图ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.示例ToolStripMenuItem,
+            this.自由画图ToolStripMenuItem});
+            this.画图ToolStripMenuItem.Name = "画图ToolStripMenuItem";
+            this.画图ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.画图ToolStripMenuItem.Text = "画图";
+            // 
+            // 示例ToolStripMenuItem
+            // 
+            this.示例ToolStripMenuItem.Name = "示例ToolStripMenuItem";
+            this.示例ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.示例ToolStripMenuItem.Text = "示例";
+            this.示例ToolStripMenuItem.Click += new System.EventHandler(this.示例ToolStripMenuItem_Click);
+            // 
+            // 自由画图ToolStripMenuItem
+            // 
+            this.自由画图ToolStripMenuItem.Name = "自由画图ToolStripMenuItem";
+            this.自由画图ToolStripMenuItem.Size = new System.Drawing.Size(152, 26);
+            this.自由画图ToolStripMenuItem.Text = "自由画图";
+            this.自由画图ToolStripMenuItem.Click += new System.EventHandler(this.自由画图ToolStripMenuItem_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(0, 31);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(882, 523);
+            this.webBrowser1.TabIndex = 1;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(762, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "清屏";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(744, 488);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.VisibleChanged += new System.EventHandler(this.pictureBox1_VisibleChanged);
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 554);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 显示ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 柳笑辰的GithubToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 柳笑辰的个人网站ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 画图ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 示例ToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStripMenuItem 自由画图ToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LxcApp/Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LxcApp
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Bitmap btmp;
+        Point pt1, pt2;
+        Pen mypen = new Pen(Color.Black, 3);
+        bool is_pen_down = false;
+        bool is_pen_up = true;
+        bool is_first_point = true;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Hide();
+            pictureBox1.Hide();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+
+        private void 柳笑辰的GithubToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //显示webBrowser组件
+            webBrowser1.Show();
+            //隐藏按钮
+            button1.Hide();
+            //隐藏图片层
+            pictureBox1.Hide();
+            //跳转页面
+            webBrowser1.Navigate("https://www.github.com/liuxiaochen712");
+        }
+
+        private void 柳笑辰的个人网站ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Show();
+            button1.Hide();
+            pictureBox1.Hide();
+            webBrowser1.Navigate("http://www.alxcy.xyz:8080");
+        }
+
+        private void 示例ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Hide();
+            button1.Hide();
+            pictureBox1.Hide();
+            Graphics graphics = this.CreateGraphics();
+            Pen pen = new Pen(Brushes.Black,8);
+            //柳 第一笔 横
+            Point p1 = new Point(100, 120);
+            Point p2 = new Point(180, 120);
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第二笔 竖
+            p1.X = 140;p1.Y = 80;
+            p2.X = 140;p2.Y = 300;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第三笔 撇
+            p1.X = 140;p1.Y = 120;
+            p2.X = 100; p2.Y = 250;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第四笔 捺
+            p1.X = 140;p1.Y = 120;
+            p2.X = 180;p2.Y = 250;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第五笔 撇
+            p1.X = 180; p1.Y = 90;
+            p2.X = 215; p2.Y = 80;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第六笔1 竖
+            p1.X = 182;p1.Y = 89;
+            p2.X = 182;p2.Y = 250;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第六笔2 提
+            p1.X = 182;p1.Y = 250;
+            p2.X = 215;p2.Y = 240;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第七笔1 竖
+            p1.X = 215;p1.Y = 170;
+            p2.X = 215;p2.Y = 288;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第七笔2 撇
+            p1.X = 215;p1.Y = 285;
+            p2.X = 182;p2.Y = 300;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第八笔1 横
+            p1.X = 220;p1.Y = 125;
+            p2.X = 290;p2.Y = 125;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第八笔2 竖
+            p1.X = 288;p1.Y = 123;
+            p2.X = 288;p2.Y = 252;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第八笔3 提
+            p1.X = 288;p1.Y = 248;
+            p2.X = 260;p2.Y = 230;
+            graphics.DrawLine(pen, p1, p2);
+            //柳 第九笔 竖
+            p1.X = 245;p1.Y = 125;
+            p2.X = 245;p2.Y = 300;
+            graphics.DrawLine(pen, p1, p2);
+        }
+
+        private void 自由画图ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //清屏
+            webBrowser1.Hide();
+            pictureBox1.Show();
+            button1.Show();
+            g = this.CreateGraphics();
+            //清空绘图区域
+            g.Clear(this.BackColor);
+            g.Dispose();
+            
+        }
+
+
+        private void 计算器ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //清屏
+            webBrowser1.Hide();
+            Graphics g = this.CreateGraphics() ;
+            //清空绘图区域
+            g.Clear(this.BackColor);
+            g.Dispose();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //鼠标左键按下
+            is_pen_down = true;
+            is_first_point = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //鼠标左键释放
+            is_pen_down = false;
+            is_pen_up = true;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //鼠标移动 绘制
+            if (is_first_point)
+            {
+                //将第一次的落点 标记并清除
+                pt1 = new Point(e.X, e.Y);
+                is_first_point = false;
+            }
+            if (is_pen_down)
+            {
+                //笔落下时拖动绘制线段
+                pt2 = new Point(e.X, e.Y);
+                g.DrawLine(mypen, pt1, pt2);
+                pt1 = pt2;
+                pictureBox1.Image = btmp;
+            }
+        }
+
+        private void pictureBox1_VisibleChanged(object sender, EventArgs e)
+        {
+            //当可见性改变时 进行初始化操作
+            btmp = new Bitmap(pictureBox1.Width, pictureBox1.Height) ;
+            g = Graphics.FromImage(btmp);
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
